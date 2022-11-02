e776c086f7e6e0c07bf6607dd3b1588d08f3d9ba
aeson9 <aesonaw143@gmail.com>
2022-11-02 10:33:00 +0800

    TPC + Player

---
+++ Assets/Resources/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public CharacterController mCharacterController;
+    public Animator mAnimator;
+
+    public float mWalkSpeed = 1.0f;
+    public float mRotationSpeed = 50.0f;
+
+    void Start()
+    {
+        mCharacterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        float speed = mWalkSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = mWalkSpeed * 2.0f;
+        }
+
+        if (mAnimator == null) return;
+
+        transform.Rotate(0.0f, hInput * mRotationSpeed * Time.deltaTime, 0.0f);
+
+        Vector3 forward = transform.forward;
+        forward.y = 0.0f;
+
+        mCharacterController.Move(forward * vInput * speed * Time.deltaTime);
+
+
+        mAnimator.SetFloat("PosX", 0);
+        mAnimator.SetFloat("PosZ", vInput * speed / (2.0f * mWalkSpeed));
+    }
+
+}
